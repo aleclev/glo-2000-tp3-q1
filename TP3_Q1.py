31d938b5886c03b58c58ec11b5c7333ae1ac29a7
unknown <alec.lev11@gmail.com>
2022-10-25 22:02:01 -0400

    progress

--- TP3_Q1.py
+++ TP3_Q1.py
@@ -11,6 +11,7 @@ import socket
 import sys
 from typing import NoReturn
 import argparse
+import random
 
 from click import Argument
 
@@ -26,14 +27,14 @@ def _parse_args(argv: 'list[str]') -> 'tuple[str, int]':
     - le port.
     """
     parser = argparse.ArgumentParser("desc")
-    parser.add_argument("-p", dest='port', required=True)
+    parser.add_argument("-p", "--port", dest='port', required=True)
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-s", action="store_true")
-    group.add_argument("-d", dest="destination")
+    group.add_argument("-s", "--server", action="store_true")
+    group.add_argument("-d", "--destination", dest="destination")
     
     args = parser.parse_args(argv)
 
-    return args.destination, args.port
+    return args.destination, int(args.port)
 
 
 def _generate_modulus_base(destination: socket.socket) -> 'tuple[int, int]':
@@ -47,7 +48,10 @@ def _generate_modulus_base(destination: socket.socket) -> 'tuple[int, int]':
     - le modulo,
     - la base.
     """
-    return 0, 0
+
+    modulo = glocrypto.find_prime()
+    base = random.randint(0, modulo)
+    return modulo, base
 
 
 def _receive_modulus_base(source: socket.socket) -> 'tuple[int, int]':
@@ -58,7 +62,8 @@ def _receive_modulus_base(source: socket.socket) -> 'tuple[int, int]':
     - le modulo,
     - la base.
     """
-    return 0, 0
+    msg = glosocket.recv_msg(socket).split(" ")
+    return msg[0], msg[1]
 
 
 def _compute_keys(modulus: int, base: int) -> 'tuple[int, int]':
@@ -99,9 +104,25 @@ def _server(port: int) -> NoReturn:
     socket_serveur.bind(("127.0.0.1", port))
     socket_serveur.listen(5)
     print(f"Ecoute sur le port : {port}")
+    
+    client_num = 0
 
     while True:
-        pass
+        (client_soc, client_addr) = socket_serveur.accept()
+        # On affiche le nombre de connexions au serveur 
+        print(f"Connexion n°{client_num}") 
+        client_num += 1 
+        # Le serveur prépare et envoie un message encodé en UTF-8 
+        GREETING = "Bienvenue sur le serveur, quel est votre nom ?\n" 
+        client_soc.send(GREETING.encode(encoding='utf-8')) 
+        # Le serveur attends une réponse d'au plus 32 octets de la 
+        # # part du client, la décode et la stocke dans une variable 
+        client_reply = client_soc.recv(32).decode(encoding='utf-8') 
+        print("Réponse: " + client_reply) # On répond au client 
+        server_reply = "Enchanté, " + client_reply 
+        client_soc.send(server_reply.encode(encoding='utf-8')) 
+        # De la même manière qu'un fichier, on ferme le # socket client une fois qu'on en a fini 
+        client_soc.close()
 
 
 def _client(destination: str, port: int) -> None:
